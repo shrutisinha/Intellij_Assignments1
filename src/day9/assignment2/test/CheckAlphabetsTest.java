432c3de2043182c312c24ea856871397078cb8c9
shrutisinha <shrutisinhaa952@gmail.com>
2017-07-13 00:04:20 +0530

    modified

--- src/day9/assignment2/test/CheckAlphabetsTest.java
+++ src/day9/assignment2/test/CheckAlphabetsTest.java
@@ -1,10 +1,9 @@
 package day9.assignment2.test;
 
 import day9.assignment2.solution.CheckAlphabets;
-import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test class to test CheckAlphabets class
